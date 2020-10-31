7551b41f9f2dff4a0e6d5a604a759fb7f35a4246
Attila Kanto <attila.kanto@gmail.com>
2020-10-31 13:56:29 +0100

    Cleanup structured events

--- structuredevent-model/src/main/java/com/sequenceiq/cloudbreak/structuredevent/event/RdsDetails.java
+++ structuredevent-model/src/main/java/com/sequenceiq/cloudbreak/structuredevent/event/RdsDetails.java
@@ -9,18 +9,23 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RdsDetails implements Serializable {
 
+    @Deprecated
     private Long id;
 
+    @Deprecated
     private String name;
 
+    @Deprecated
     private String description;
 
+    @Deprecated
     private String connectionURL;
 
     private String sslMode;
 
     private String databaseEngine;
 
+    @Deprecated
     private String connectionDriver;
 
     private Long creationDate;
@@ -31,65 +36,22 @@ public class RdsDetails implements Serializable {
 
     private String type;
 
+    @Deprecated
     private String connectorJarUrl;
 
+    @Deprecated
     private Long workspaceId;
 
+    @Deprecated
     private String userId;
 
+    @Deprecated
     private String userName;
 
+    @Deprecated
     private String tenantName;
 
     private Boolean externalDatabase;
-
-    public String getTenantName() {
-        return tenantName;
-    }
-
-    public void setTenantName(String tenantName) {
-        this.tenantName = tenantName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getConnectionURL() {
-        return connectionURL;
-    }
-
-    public void setConnectionURL(String connectionURL) {
-        this.connectionURL = connectionURL;
-    }
-
-    public String getSslMode() {
-        return sslMode;
-    }
-
-    public void setSslMode(String sslMode) {
-        this.sslMode = sslMode;
-    }
 
     public String getDatabaseEngine() {
         return databaseEngine;
@@ -97,14 +59,6 @@ public class RdsDetails implements Serializable {
 
     public void setDatabaseEngine(String databaseEngine) {
         this.databaseEngine = databaseEngine;
-    }
-
-    public String getConnectionDriver() {
-        return connectionDriver;
-    }
-
-    public void setConnectionDriver(String connectionDriver) {
-        this.connectionDriver = connectionDriver;
     }
 
     public Long getCreationDate() {
@@ -139,38 +93,6 @@ public class RdsDetails implements Serializable {
         this.type = type;
     }
 
-    public String getConnectorJarUrl() {
-        return connectorJarUrl;
-    }
-
-    public void setConnectorJarUrl(String connectorJarUrl) {
-        this.connectorJarUrl = connectorJarUrl;
-    }
-
-    public Long getWorkspaceId() {
-        return workspaceId;
-    }
-
-    public void setWorkspaceId(Long workspaceId) {
-        this.workspaceId = workspaceId;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
     public Boolean getExternal() {
         return externalDatabase;
     }
@@ -179,4 +101,11 @@ public class RdsDetails implements Serializable {
         externalDatabase = external;
     }
 
+    public String getSslMode() {
+        return sslMode;
+    }
+
+    public void setSslMode(String sslMode) {
+        this.sslMode = sslMode;
+    }
 }
